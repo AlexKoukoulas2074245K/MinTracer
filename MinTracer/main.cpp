191fbcdfd1a936d4ab749a7d04e6fc47a0fb5f3b
Alexios <alex.koukoulas.1994@gmail.com>
2017-11-23 15:29:52 +0000

    Created Project

---
+++ MinTracer/main.cpp
@@ -0,0 +1,11 @@
+/**********************************************************************/
+/** main.cpp by Alex Koukoulas (C) 2017 All Rights Reserved  **/
+/** File Description:                                                **/
+/**********************************************************************/
+
+#pragma once
+
+int main(int argc, char* arv[])
+{
+	return 0;
+}
